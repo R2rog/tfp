dd39af52d8d336a1f2b826d5d4e241ddfada3268
Arturo Guevara <sirR2rog@gmail.com>
2022-07-05 20:51:18 -0500

    The board now also changes the icon background image when the champion gets moved or removed from the board.

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -16,6 +16,120 @@ export default{
                 [{},{},{},{},{},{},{}],
                 [{},{},{},{},{},{},{}]
             ],
+            boardTraits:{
+                assassin: {
+                    name: 'Assassin',
+                    value: 0
+                },
+                bard:{
+                    name: 'Bard',
+                    value: 0
+                },
+                bruiser: {
+                    name: 'Bruiser',
+                    value: 0
+                },
+                cannoneer: {
+                    name: 'Cannoneer',
+                    value: 0
+                },
+                cavalier: {
+                    name: 'Cavalier',
+                    value: 0
+                },
+                dragon: {
+                    name: 'Dragon',
+                    value: 0
+                },
+                dragonmancer: {
+                    name: 'Dragonmancer',
+                    value: 0
+                },
+                evoker: {
+                    name: 'Evoker',
+                    value: 0
+                },
+                guardian: {
+                    name: 'Guardian',
+                    value: 0
+                },
+                legend: {
+                    name: 'Legend',
+                    value: 0
+                },
+                mage: {
+                    name: 'Mage',
+                    value: 0
+                },
+                mystic: {
+                    name: 'Mystic',
+                    value: 0
+                },
+                shapeshifter:{
+                    name: 'Shapeshifter',
+                    value: 0
+                },
+                spellthief:{
+                    name: 'Spellthief',
+                    value: 0
+                },
+                starcaller:{
+                    name: 'Starcaller',
+                    value: 0
+                },
+                swiftshot: {
+                    name: 'Swiftshot',
+                    value: 0
+                },
+                warrior:{
+                    name: 'Warrior',
+                    value: 0
+                },
+                astral: {
+                    name: 'Astral',
+                    value: 0
+                },
+                guild: {
+                    name: 'Guild',
+                    value: 0
+                },
+                jade: {
+                    name: 'Jade',
+                    value: 0
+                },
+                mirage: {
+                    name: 'Mirage',
+                    value: 0
+                },
+                ragewing: {
+                    name: 'Ragewing',
+                    value: 0
+                },
+                revel: {
+                    name: 'Revel',
+                    value: 0
+                },
+                scalescorn: {
+                    name: 'Scalescorn',
+                    value: 0
+                },
+                shimmerscale: {
+                    name: 'Shimmerscale',
+                    value: 0
+                },
+                tempest: {
+                    name: 'Tempest',
+                    value:0
+                },
+                trainer: {
+                    name: 'Trainer',
+                    value: 0
+                },
+                whispers: {
+                    name: 'Whispers',
+                    value: 0
+                }
+            },
             boardClasses:{
                 assassin: {
                     name: 'Assassin',
@@ -116,7 +230,7 @@ export default{
                     value: 0
                 },
                 shimmerscale: {
-                    name: 'Shimerscale',
+                    name: 'Shimmerscale',
                     value: 0
                 },
                 tempest: {
@@ -143,6 +257,7 @@ export default{
                     break;
                 };
             };
+            console.log('Selected class: ', this.selectedClass)
         },
         originInfo: function(selectedTrait){
             let originArr = this.propsJSON.originsArr;
@@ -151,7 +266,8 @@ export default{
                     this.selectedOrigin = originArr[i];
                     break;
                 };
-            };            
+            }; 
+            console.log('Selected origin: ', this.selectedOrigin)           
         },
         hideInfo: function(){
             this.selectedClass = '';
@@ -162,30 +278,44 @@ export default{
             let i = e.target.id[0]-1;
             let j = e.target.id[1]-1;
             this.selectedChamp = this.boardValues[i][j];
-            this.selectedChamp.classes.forEach(element => {
-                let lowCaseEl = element.toLowerCase();
-                this.boardClasses[lowCaseEl].value = this.boardClasses[lowCaseEl].value - 1;
+            this.selectedChamp.traits.forEach(trait=>{
+                let lowCaseTrait = trait.toLowerCase();
+                this.boardTraits[lowCaseTrait].value = this.boardTraits[lowCaseTrait].value - 1;
+                let value = this.boardTraits[lowCaseTrait].value;
+                let setArr = [];
+                if(this.propsJSON.traitsArr.hasOwnProperty(lowCaseTrait)) setArr = this.propsJSON.traitsArr[lowCaseTrait].sets;
+                else setArr = this.propsJSON.originsArr[lowCaseTrait].sets;
+                let classIcon=document.getElementById(trait+'-img'); 
+                for (let i = 0; i < setArr.length; i++) {
+                    if(value>=setArr[i].min && value<setArr[i+1].min){
+                        let borderURL = 'url(./src/assets/icons/set7/traits/'+setArr[i].style+'.svg)';
+                        classIcon.style.backgroundImage = borderURL;
+                    };
+                };
             });
-            this.selectedChamp.origins.forEach(origin => {
-                let lowCaseOg = origin.toLowerCase();
-                this.boardOrigins[lowCaseOg].value = this.boardOrigins[lowCaseOg].value - 1;
-            });
+            this.seleted
             this.boardValues[i][j]= {};
             this.innerDragEvent = true;
         },
         champDrop: function(e){
             if(this.innerDragEvent == false)this.selectedChamp = this.champDragged;
-            console.log('Selected champ: ',this.selectedChamp );
             let i = e.target.id[4]-1;
             let j = e.target.id[11]-1;
             let target = document.getElementById(e.target.id);
-            this.selectedChamp.classes.forEach(element => {
-                let lowCaseEl = element.toLowerCase();
-                this.boardClasses[lowCaseEl].value = this.boardClasses[lowCaseEl].value + 1;
-            });
-            this.selectedChamp.origins.forEach(origin => {
-                let lowCaseOg = origin.toLowerCase();
-                this.boardOrigins[lowCaseOg].value = this.boardOrigins[lowCaseOg].value + 1;
+            this.selectedChamp.traits.forEach(trait=>{
+                let lowCaseTrait = trait.toLowerCase();
+                this.boardTraits[lowCaseTrait].value = this.boardTraits[lowCaseTrait].value + 1;
+                let value = this.boardTraits[lowCaseTrait].value;
+                let setArr = [];
+                if(this.propsJSON.traitsArr.hasOwnProperty(lowCaseTrait)) setArr = this.propsJSON.traitsArr[lowCaseTrait].sets;
+                else setArr = this.propsJSON.originsArr[lowCaseTrait].sets;
+                let classIcon=document.getElementById(trait+'-img'); 
+                for (let i = 0; i < setArr.length; i++) {
+                    if(value>=setArr[i].min && value<setArr[i+1].min){
+                        let borderURL = 'url(./src/assets/icons/set7/traits/'+setArr[i].style+'.svg)';
+                        classIcon.style.backgroundImage = borderURL;
+                    };
+                };
             });
             if(target.hasChildNodes()) target.children[0].src = this.selectedChamp.icon;
             else target.src = this.selectedChamp.icon;
@@ -200,24 +330,20 @@ export default{
 <template>
    <div id="board">
         <div id="synergies">
-            <div v-for="element in boardClasses" v-show="element.value > 0"  :key="element">
-                <h2 v-on:mouseover="classInfo(element.name)"
-                    v-on:mouseleave="hideInfo">
-                    {{element.name}}: {{element.value}}
-                </h2>
-                <span class="synergies-tooltip" v-show="this.selectedClass.name == element.name">
-                    <h2>{{this.selectedClass.bonus}}</h2>
-                    <h2 v-for="buff in this.selectedClass.set" :key="buff.count">{{buff.count}}: {{buff.bonus}}</h2>
-                </span>
-            </div>
-            <div v-for="origin in boardOrigins" v-show="origin.value > 0" :key="origin">
-                <h2 v-on:mouseover="originInfo(origin.name)"
-                    v-on:mouseleave="hideInfo">
-                    {{origin.name}}: {{origin.value}}
-                </h2>
-                <span class="synergies-tooltip" v-show="this.selectedOrigin.name == origin.name">
-                    <h2>{{this.selectedOrigin.bonus}}</h2>
-                    <h2 v-for="buff in this.selectedOrigin.set" :key="buff.count">{{buff.count}}: {{buff.bonus}}</h2>
+            <div v-for="trait in boardTraits" v-show="trait.value > 0"  :key="trait">
+                <div class="class-tag">
+                    <img v-bind:src="'./src/assets/icons/set7/traits/'+trait.name+'.svg'" 
+                        v-bind:id="trait.name + '-img'"
+                        v-bind:class="'trait-logo'"
+                        v-on:mouseover="classInfo(trait.name)"
+                        v-on:mouseleave="hideInfo"/>
+                    <h2>
+                        {{trait.name}}: {{trait.value}}
+                    </h2>
+                </div>
+                <span class="synergies-tooltip" v-show="this.selectedClass.name == trait.name">   
+                    <h2>{{this.selectedClass.description}}</h2>
+                    <h2 v-for="buff in this.selectedClass.sets" :key="buff.count">{{buff.min}}: {{buff.buff}}</h2>
                 </span>
             </div>
         </div>
@@ -256,6 +382,18 @@ export default{
     position:absolute;
     z-index: 2;
     width: 350%;
+}
+.trait-logo{
+    height: 1.5rem;
+}
+.class-tag{
+    display: flex;
+}
+#Assassin-img{
+    background-image: url('./src/assets/icons/set7/traits/gold.svg');
+}
+.origin-tag{
+    display: flex;
 }
 .board-row{
     background-color: cornflowerblue;
