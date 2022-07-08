4eeafac8cecf2b3d245c19033ab0c62e51669b4e
Arturo Guevara <sirR2rog@gmail.com>
2022-07-07 19:04:11 -0500

    Bug fixed where the board didn't showed the synergies tooltip

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -6,7 +6,7 @@ export default{
     data(){
         return{
             selectedChamp : {},
-            selectedClass: {},
+            selectedTrait: {},
             innerDragEvent: false,
             selectedOrigin: {},
             champCounter: 0,
@@ -129,135 +129,16 @@ export default{
                     name: 'Whispers',
                     value: 0
                 }
-            },
-            boardClasses:{
-                assassin: {
-                    name: 'Assassin',
-                    value: 0
-                },
-                bard:{
-                    name: 'Bard',
-                    value: 0
-                },
-                bruiser: {
-                    name: 'Bruiser',
-                    value: 0
-                },
-                cannoneer: {
-                    name: 'Cannoneer',
-                    value: 0
-                },
-                cavalier: {
-                    name: 'Cavalier',
-                    value: 0
-                },
-                dragon: {
-                    name: 'Dragon',
-                    value: 0
-                },
-                dragonmancer: {
-                    name: 'Dragonmancer',
-                    value: 0
-                },
-                evoker: {
-                    name: 'Evoker',
-                    value: 0
-                },
-                guardian: {
-                    name: 'Guardian',
-                    value: 0
-                },
-                legend: {
-                    name: 'Legend',
-                    value: 0
-                },
-                mage: {
-                    name: 'Mage',
-                    value: 0
-                },
-                mystic: {
-                    name: 'Mystic',
-                    value: 0
-                },
-                shapeshifter:{
-                    name: 'Shapeshifter',
-                    value: 0
-                },
-                spellthief:{
-                    name: 'Spellthief',
-                    value: 0
-                },
-                starcaller:{
-                    name: 'Starcaller',
-                    value: 0
-                },
-                swiftshot: {
-                    name: 'Swiftshot',
-                    value: 0
-                },
-                warrior:{
-                    name: 'Warrior',
-                    value: 0
-                }
-            },
-            boardOrigins:{
-                astral: {
-                    name: 'Astral',
-                    value: 0
-                },
-                guild: {
-                    name: 'Guild',
-                    value: 0
-                },
-                jade: {
-                    name: 'Jade',
-                    value: 0
-                },
-                mirage: {
-                    name: 'Mirage',
-                    value: 0
-                },
-                ragewing: {
-                    name: 'Ragewing',
-                    value: 0
-                },
-                revel: {
-                    name: 'Revel',
-                    value: 0
-                },
-                scalescorn: {
-                    name: 'Scalescorn',
-                    value: 0
-                },
-                shimmerscale: {
-                    name: 'Shimmerscale',
-                    value: 0
-                },
-                tempest: {
-                    name: 'Tempest',
-                    value:0
-                },
-                trainer: {
-                    name: 'Trainer',
-                    value: 0
-                },
-                whispers: {
-                    name: 'Whispers',
-                    value: 0
-                }
             }
         }
     },
     methods:{
-        classInfo: function(selectedTrait){
-            let classArr = this.propsJSON.traitsArr;
-            for (let i = 0; i < classArr.length; i++) {
-                if(classArr[i].name == selectedTrait){
-                    this.selectedClass = classArr[i];
-                    break;
-                };
-            };
-            console.log('Selected class: ', this.selectedClass)
+        traitInfo: function(selectedTrait){
+            console.log('Selected trait param', selectedTrait)
+            let traitsArr= [];
+            if(this.propsJSON.traitsArr.hasOwnProperty(selectedTrait.toLowerCase())) traitsArr = this.propsJSON.traitsArr;
+            else traitsArr = this.propsJSON.originsArr; 
+            this.selectedTrait = traitsArr[selectedTrait.toLowerCase()];
         },
         originInfo: function(selectedTrait){
             let originArr = this.propsJSON.originsArr;
@@ -267,10 +148,9 @@ export default{
                     break;
                 };
             }; 
-            console.log('Selected origin: ', this.selectedOrigin)           
         },
         hideInfo: function(){
-            this.selectedClass = '';
+            this.selectedTrait = '';
             this.selectedOrigin = '';   
         },
         innerDrag: function(e){
@@ -335,15 +215,15 @@ export default{
                     <img v-bind:src="'./src/assets/icons/set7/traits/'+trait.name+'.svg'" 
                         v-bind:id="trait.name + '-img'"
                         v-bind:class="'trait-logo'"
-                        v-on:mouseover="classInfo(trait.name)"
+                        v-on:mouseover="traitInfo(trait.name)"
                         v-on:mouseleave="hideInfo"/>
                     <h2>
                         {{trait.name}}: {{trait.value}}
                     </h2>
                 </div>
-                <span class="synergies-tooltip" v-show="this.selectedClass.name == trait.name">   
-                    <h2>{{this.selectedClass.description}}</h2>
-                    <h2 v-for="buff in this.selectedClass.sets" :key="buff.count">{{buff.min}}: {{buff.buff}}</h2>
+                <span class="synergies-tooltip" v-show="this.selectedTrait.name == trait.name">   
+                    <h2>{{this.selectedTrait.description}}</h2>
+                    <h2 v-for="buff in this.selectedTrait.sets" :key="buff.count">{{buff.min}}: {{buff.buff}}</h2>
                 </span>
             </div>
         </div>
@@ -377,7 +257,8 @@ export default{
     font-size: 0.8rem;
 }
 .synergies-tooltip{
-    background-color: grey;
+    background-color: aquamarine;
+    color: black;
     font-size: 0.80rem;
     position:absolute;
     z-index: 2;
@@ -388,9 +269,6 @@ export default{
 }
 .class-tag{
     display: flex;
-}
-#Assassin-img{
-    background-image: url('./src/assets/icons/set7/traits/gold.svg');
 }
 .origin-tag{
     display: flex;
